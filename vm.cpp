04d2dfc7c232068444f94b909dccf178a456aa8e
Petr Faltýnek <pfaltynek@alliancels.cz>
2017-08-16 14:32:30 +0200

    instructions implementation continues

--- vm.cpp
+++ vm.cpp
@@ -1,6 +1,9 @@
 #include "vm.h"
 #include <iostream>
 
+#define FIRST_REGISTER_INDEX_VALUE 32768
+#define LAST_REGISTER_INDEX_VALUE 32775
+
 bool cVM::get_input_value(std::ifstream &input, unsigned short &value) {
 	bool result = true;
 	unsigned char v[2];
@@ -34,7 +37,7 @@ cVM::cVM(std::ifstream &input) {
 	while (!_stack.empty()) {
 		_stack.pop();
 	}
-	_memory.clear();
+	//_memory.clear();
 	_bin_code.clear();
 
 	while (get_input_value(input, value)) {
@@ -43,17 +46,44 @@ cVM::cVM(std::ifstream &input) {
 }
 
 bool cVM::IsValueValid(unsigned short value) {
-	return (value >= 32776);
+	return (value <= LAST_REGISTER_INDEX_VALUE);
 }
 
 bool cVM::IsValueRegister(unsigned short value) {
-	return ((value >= 32768) && (value <= 32775));
+	return ((value >= FIRST_REGISTER_INDEX_VALUE) && (value <= LAST_REGISTER_INDEX_VALUE));
+}
+
+bool cVM::IsValueMemoryAddress(unsigned short value) {
+	return (value < FIRST_REGISTER_INDEX_VALUE);
+}
+
+bool cVM::GetSourceValue(unsigned short src_value, unsigned short &result) {
+	if (IsValueValid(src_value)) {
+		if (IsValueRegister(src_value)) {
+			result = _regs[src_value - FIRST_REGISTER_INDEX_VALUE];
+		} else {
+			//result = _memory[src_value];
+			result = src_value;
+		}
+		return true;
+	} else {
+		return false;
+	}
+}
+
+bool cVM::SetTargetValue(unsigned short storage, unsigned short value) {
+	if (IsValueRegister(storage)) {
+		_regs[storage - FIRST_REGISTER_INDEX_VALUE] = value;
+		return true;
+	} else {
+		return false;
+	}
 }
 
 TRACE_FINISH_REASON cVM::RunBinCode() {
 	TRACE_FINISH_REASON result = TRACE_FINISH_REASON::UNKNOWN;
 	bool terminated = false;
-	unsigned short op1, op2, op3;
+	unsigned short op1, op2, op3, tmp;
 
 	while (_pc < _bin_code.size() && !terminated) {
 		switch ((INSTRUCTIONS)_bin_code[_pc]) {
@@ -62,18 +92,267 @@ TRACE_FINISH_REASON cVM::RunBinCode() {
 				result = TRACE_FINISH_REASON::HALTED;
 				terminated = true;
 				break;
-			case INSTRUCTIONS::JMP:
-				_pc++;
-				_pc = _bin_code[_pc];
-				break;
-			case INSTRUCTIONS::JT:
+			case INSTRUCTIONS::SET:
 				_pc++;
 				op1 = _bin_code[_pc];
 				_pc++;
-				op2 = _bin_code[_pc];
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					if (SetTargetValue(op1, op2)) {
+						_pc++;
+					} else {
+						result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::PUSH:
 				_pc++;
-				if (IsValueValid(op1) && IsValueValid(op2)) {
-
+				if (GetSourceValue(_bin_code[_pc], op1)) {
+					_stack.push(op1);
+					_pc++;
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::POP:
+				if (_stack.empty()) {
+					result = TRACE_FINISH_REASON::STACK_EMPTY;
+					terminated = true;
+					break;
+				}
+				_pc++;
+				op1 = _bin_code[_pc];
+				op2 = _stack.top();
+				_stack.pop();
+				if (SetTargetValue(op1, op2)) {
+					_pc++;
+				} else {
+					result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::EQ:
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = (op2 == op3 ? 1 : 0);
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::GT:
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = (op2 > op3 ? 1 : 0);
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::JMP:
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op1)) {
+					_pc = op1;
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::JT:
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op1)) {
+					_pc++;
+					if (op1) {
+						if (GetSourceValue(_bin_code[_pc], op2)) {
+							_pc = op2;
+						} else {
+							result = TRACE_FINISH_REASON::INVALID_NUMBER;
+							terminated = true;
+						}
+					} else {
+						_pc++;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::JF:
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op1)) {
+					_pc++;
+					if (!op1) {
+						if (GetSourceValue(_bin_code[_pc], op2)) {
+							_pc = op2;
+						} else {
+							result = TRACE_FINISH_REASON::INVALID_NUMBER;
+							terminated = true;
+						}
+					} else {
+						_pc++;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::ADD:
+				tmp = 0;
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = (op2 + op3) % FIRST_REGISTER_INDEX_VALUE;
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::MULT:
+				tmp = 0;
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = (op2 * op3) % FIRST_REGISTER_INDEX_VALUE;
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::MOD:
+				tmp = 0;
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = op2 % op3;
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::AND:
+				tmp = 0;
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = op2 & op3;
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::OR:
+				tmp = 0;
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					if (GetSourceValue(_bin_code[_pc], op3)) {
+						_pc++;
+						tmp = op2 | op3;
+						if (!SetTargetValue(op1, tmp)) {
+							result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+							terminated = true;
+						}
+					} else {
+						result = TRACE_FINISH_REASON::INVALID_NUMBER;
+						terminated = true;
+					}
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
+				break;
+			case INSTRUCTIONS::NOT:
+				tmp = 0;
+				_pc++;
+				op1 = _bin_code[_pc];
+				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op2)) {
+					_pc++;
+					tmp = ~op2 & 0x7FFF;
+					if (!SetTargetValue(op1, tmp)) {
+						result = TRACE_FINISH_REASON::REGISTER_REQUIRED;
+						terminated = true;
+					}
 				} else {
 					result = TRACE_FINISH_REASON::INVALID_NUMBER;
 					terminated = true;
@@ -81,8 +360,13 @@ TRACE_FINISH_REASON cVM::RunBinCode() {
 				break;
 			case INSTRUCTIONS::OUT:
 				_pc++;
-				std::cout << (char)_bin_code[_pc];
-				_pc++;
+				if (GetSourceValue(_bin_code[_pc], op1)) {
+					std::cout << (char)op1;
+					_pc++;
+				} else {
+					result = TRACE_FINISH_REASON::INVALID_NUMBER;
+					terminated = true;
+				}
 				break;
 			case INSTRUCTIONS::NOOP:
 				_pc++;
@@ -90,7 +374,7 @@ TRACE_FINISH_REASON cVM::RunBinCode() {
 			default:
 				result = TRACE_FINISH_REASON::UNKNOWN_INSTRUCTION;
 				std::cout << std::endl
-						  << "Unknown instruction: " << _bin_code[_pc] << std::endl;
+						  << "Unknown instruction: " << _bin_code[_pc] << " at " << _pc << std::endl;
 				terminated = true;
 				break;
 		}
--- vm.h
+++ vm.h
@@ -11,16 +11,35 @@ typedef enum {
 	HALTED,
 	UNKNOWN_INSTRUCTION,
 	INVALID_NUMBER,
+	REGISTER_REQUIRED,
 	STACK_EMPTY,
 } TRACE_FINISH_REASON;
 
 typedef enum {
 	HALT = 0,
+	SET = 1,
+	PUSH = 2,
+	POP = 3,
+	EQ = 4,
+	GT = 5,
 	JMP = 6,
 	JT = 7,
+	JF = 8,
+	ADD = 9,
+	MULT = 10,
+	MOD = 11,
+	AND = 12,
+	OR = 13,
+	NOT = 14,
+	// = 15,
+	// = 16,
+	// = 17,
+	// = 18,
 	OUT = 19,
+	// = 20,
 	NOOP = 21
-} INSTRUCTIONS;
+}
+INSTRUCTIONS;
 
 class cVM {
   public:
@@ -33,11 +52,14 @@ class cVM {
 	bool get_input_value(std::ifstream &input, unsigned short &value);
 	bool IsValueValid(unsigned short value);
 	bool IsValueRegister(unsigned short value);
+	bool IsValueMemoryAddress(unsigned short value);
+	bool GetSourceValue(unsigned short src_value, unsigned short &result);
+	bool SetTargetValue(unsigned short storage, unsigned short value);
 
   private:
 	short _pc;
 	short _regs[8];
-	std::map<unsigned short, unsigned short> _memory;
+	//std::map<unsigned short, unsigned short> _memory;
 	std::stack<unsigned short> _stack;
 	std::vector<unsigned short> _bin_code;
 };
